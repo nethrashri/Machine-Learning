ab290d3ccc0f0310bd9f0732a588d406d73219d1
Nethrashri <36656522+nethrashri@users.noreply.github.com>
2020-04-01 00:45:07 +0530

    Add files via upload

---
+++ Pattern_ Program_Module/Run.py
@@ -0,0 +1,21 @@
+
+
+from A import Alpha_A
+from B import Alpha_B
+from C import Alpha_C
+from D import Alpha_D
+from E import Alpha_E
+from F import Alpha_F
+from G import Alpha_G
+from H import Alpha_H
+
+
+
+Alpha_A.alpha_a()
+Alpha_B.alpha_b()
+Alpha_C.alpha_c()
+Alpha_D.alpha_d()
+Alpha_E.alpha_e()
+Alpha_F.alpha_f()
+Alpha_G.alpha_g()
+Alpha_H.alpha_h()
